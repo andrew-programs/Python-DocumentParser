ca8c64099e46373af4c523246ea029a1196f6818
Andrew Williams <110430206+andrew-programs@users.noreply.github.com>
2022-11-08 12:20:49 -0700

    refactored and adding finishing code
    probably has quite the amount of bugs though

--- DocumentParser.py
+++ DocumentParser.py
@@ -9,13 +9,15 @@ class DocumentParser:
 
         for line in lineList:
             wordList.extend(line.rsplit(" "))
-            
+        
+        for index in range(len(wordList)):
+            wordList[index] = wordList[index].lower()
         return wordList
 
     # Take a list of words and count the number of times the word was used
     def wordCount(self, wordList: list[str]) -> list[list[str, int]]:
         """
-        A method that counts how many times each word has occured and returns a list of lists of strs and ints.
+        A method that counts how many times each word has occured and returns a list of lists of strs and ints in descending order.
         [[word, count], [word, count]].
         """
         wordSet = set(wordList)
@@ -30,13 +32,15 @@ class DocumentParser:
             
             if word != "":
                 wordCount.append([word, count])
+            
+            self.__statSort(wordCount)
         
         return wordCount
 
-    
+    # Private method to remove punct from single word.
     def __removePunct(self, word: str) -> str:
         """Private method to remove punctuations from a word, feel free to use it for a singular word."""
-        PUNCT_TO_REMOVE = "!@#$%^&*()_+=[]\{}|;':,./<>?\n\\"
+        PUNCT_TO_REMOVE = "!@#$%^&*()_+=[]\{}|;':,./<>?\n\\\t"
 
         chars, newWord = [char for char in word if char not in PUNCT_TO_REMOVE], ""
 
@@ -45,6 +49,24 @@ class DocumentParser:
         
         return newWord
 
+    # Method to remove punct from multiple words in a list.
     def removePuncts(self, words: list[str]) -> list[str]:
         """A method to remove punctuation from a list of words."""
         return [self.__removePunct(word) for word in words]
+    
+    # Private method to sort word statistics.
+    def __statSort(self, list: list) -> list:
+        """A private method that sorts out a list using selection sorting algorithm, O(n^2)"""
+        for index in range(len(list)):
+            minimumIntegerIndex = index
+
+            for secondIndex in range(len(list)):
+                if index < secondIndex:
+                    if list[secondIndex][1] > list[minimumIntegerIndex][1]:
+                        minimumIntegerIndex = secondIndex
+            
+            list.insert(index, list.pop(minimumIntegerIndex))
+        return list
+    
+    def totalWordCount(self, wordList: list[str]) -> int:
+        return len(wordList)
