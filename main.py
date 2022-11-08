e7529bdca07e5e41711b535a2f9859161b4be8d1
Andrew Williams <110430206+andrew-programs@users.noreply.github.com>
2022-11-08 12:21:22 -0700

    refactored and added code

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from DocumentParser import *
+# ^^ Please don't do this on a regular basis
+
 # Main example function
 def main() -> None:
-    pass
+    parser = DocumentParser()
+
+    words = parser.parseFile("./documents/document2.txt")
+    words = parser.removePuncts(words)
+    words = parser.wordCount(words)
+
+    print(f"[Word Count is {parser.totalWordCount(words)}]")
+    for word, count in words:
+        print(f"{word:>14} : {count:3d}")
+
 
 if __name__ == "__main__":
     main()
